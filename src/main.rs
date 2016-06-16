51941c1a728741dda9b951cc1d4ac119b60b48bd
Jonas Schievink <jonas@schievink.net>
2016-06-16 21:12:24 +0200

    Use "cargo metadata" to collect local packages
    Much simpler, much cleaner, much more official, works with optional deps
    (and features).

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 #[macro_use] extern crate log;
-extern crate toml;
-extern crate rustc_serialize;
-extern crate walkdir;
+extern crate clap;
+extern crate serde_json;
 
-mod lockfile;
-mod manifest;
+use clap::{ArgMatches, App, Arg, AppSettings};
+use serde_json::Value;
 
-use walkdir::WalkDir;
-
-use std::env::current_dir;
-use std::fs::File;
-use std::io::{self, Read, Write, ErrorKind};
-use std::process::{self, Command, ExitStatus};
-use std::path::PathBuf;
-use std::fmt;
-use std::env;
-use std::iter;
+use std::io::{self, Write};
+use std::process::{self, Command};
+use std::error::Error;
+use std::str::FromStr;
 
 #[cfg(feature = "logging")]
 fn init_logging() {
@@ -26,148 +19,58 @@ fn init_logging() {
 #[cfg(not(feature = "logging"))]
 fn init_logging() {}
 
+/// Collects local packages and returns them as a vector of package names to be passed to
+/// `cargo -p <pkg>`.
+fn collect_local_pkgs(cargo_args: &[&str]) -> Result<Vec<String>, Box<Error>> {
+    let mut cargo = Command::new("cargo");
+    cargo.arg("metadata");
+    cargo.args(cargo_args);
+    debug!("invoking {:?}", cargo);
 
-/// The kinds of errors that can occur in `run()`.
-#[derive(Debug)]
-enum RunError {
-    /// The lock file didn't exist
-    LockFileNotFound,
-    /// Lock file exists, but is invalid
-    LockFileError(lockfile::ReadError),
-    /// I/O error when trying to open the given file
-    ///
-    /// (less specific than `LockFileNotFound`, but more specific then `IoError`)
-    FileError(String, io::Error),
-    /// Unknown I/O error
-    IoError(io::Error),
-    /// Invalid command line arguments (with description)
-    InvalidArgument(String),
-    /// Invoked subcommand returned an error
-    ExecError(ExitStatus),
-    /// Error while reading a `Cargo.toml`
-    ManifestError(manifest::ReadError),
-    /// Probably missed a package
-    MissedPkg {
-        /// Name of the missed package
-        package: String,
-        /// Path to the `Cargo.toml`
-        manifest: PathBuf,
-    },
-}
+    let stdout = String::from_utf8(try!(cargo.output()).stdout).unwrap();
+    debug!("cargo metadata: {}", stdout);
 
-impl From<io::Error> for RunError {
-    fn from(e: io::Error) -> Self {
-        RunError::IoError(e)
-    }
-}
+    let mut local_pkgs = Vec::new();
 
-impl From<lockfile::ReadError> for RunError {
-    fn from(e: lockfile::ReadError) -> Self {
-        RunError::LockFileError(e)
-    }
-}
+    let main_obj = try!(Value::from_str(&stdout));
+    let main_obj = main_obj.as_object().unwrap();
+    let pkg_array = main_obj["packages"].as_array().unwrap();
+    for pkg_obj in pkg_array {
+        let pkg_obj = pkg_obj.as_object().unwrap();
 
-impl From<manifest::ReadError> for RunError {
-    fn from(e: manifest::ReadError) -> Self {
-        RunError::ManifestError(e)
-    }
-}
-
-impl fmt::Display for RunError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            RunError::LockFileNotFound => write!(f, "lock file 'Cargo.lock' not found (try running \
-                                                     `cargo generate-lockfile` first)"),
-            RunError::LockFileError(ref e) => write!(f, "error reading lockfile: {}", e),
-            RunError::FileError(ref name, ref e) => write!(f, "error opening '{}': {}", name, e),
-            RunError::IoError(ref e) => write!(f, "{}", e),
-            RunError::InvalidArgument(ref e) => write!(f, "invalid arguments: {}", e),
-            RunError::ExecError(status) => write!(f, "command exited with error: {}", status),
-            RunError::ManifestError(ref e) => write!(f, "error reading manifest: {}", e),
-            RunError::MissedPkg { ref package, ref manifest } =>
-                // FIXME: Better error message
-                write!(f, "probably missed package {} (from {})", package, manifest.display()),
+        if pkg_obj["source"].is_null() {
+            let name = pkg_obj["name"].as_string().unwrap();
+            local_pkgs.push(name.into());
         }
     }
+
+    Ok(local_pkgs)
 }
 
 /// Main entry point.
-fn run() -> Result<(), RunError> {
-    // First, validate cmd line arguments
-    let mut args = env::args();
-    // We can dispose of the first 2 arguments (the executable, and the subcommand passed by cargo)
-    args.next().expect("exec name not passed as argument");
-    if args.next() == None {
-        return Err(RunError::InvalidArgument("subcommand not passed as argument (not running with \
-                                              cargo?)".into()));
-    }
-    // Then we expect at least one more argument to pass to cargo
-    let cargo_cmd = match args.next() {
-        Some(cmd) => cmd,
-        None => return Err(RunError::InvalidArgument("no cargo command passed".into())),
-    };
-    // Any other arg is passed along to cargo
-    let cargo_args: Vec<_> = args.collect();
+fn run(args: &ArgMatches) -> Result<(), Box<Error>> {
+    let (cargo_cmd, subcmd_args) = args.subcommand();
+    info!("subcmd: {} {:?}", cargo_cmd, subcmd_args);
+    let mut arg_iter = subcmd_args.unwrap().values_of(cargo_cmd).unwrap();
+    let cargo_cmd = arg_iter.next().unwrap();
+    let subcmd_args = arg_iter.collect::<Vec<_>>();
+    let cargo_args = args.value_of("CARGO_ARGS").map(|cargs| cargs.split_whitespace().collect::<Vec<_>>()).unwrap_or(subcmd_args);
 
     // Helper for running Cargo on a package
-    let run_cargo = |pkg: &str| -> Result<(), RunError> {
+    let run_cargo = |pkg: &str| -> Result<(), Box<Error>> {
         let mut cmd = Command::new("cargo");
         cmd.arg(&cargo_cmd).arg("-p").arg(pkg).args(&cargo_args);
         info!("running {:?}", cmd);
         let status = try!(cmd.status());
 
         if !status.success() {
-            return Err(RunError::ExecError(status));
+            return Err(format!("subcommand exited with error code: {:?}", status).into());
         }
 
         Ok(())
     };
 
-    let mut file = try!(File::open("Cargo.lock").map_err(|e| match e.kind() {
-        ErrorKind::NotFound => {
-            RunError::LockFileNotFound
-        }
-        _ => {
-            RunError::FileError(String::from("Cargo.lock"), e)
-        }
-    }));
-    let mut string = String::new();
-    try!(file.read_to_string(&mut string));
-    let lockfile = try!(lockfile::parse(&string));
-    debug!("parsed lockfile: {:#?}", lockfile);
-    info!("root pkg: {}", &lockfile.root.name);
-
-    // First, collect all local packages, including the main package
-    let local_pkgs = iter::once(&lockfile.root.name)
-                          .chain(lockfile.package
-                                 .iter()
-                                 .filter(|pkg| pkg.is_local())
-                                 .map(|pkg| &pkg.name))
-                          .collect::<Vec<_>>();
-    debug!("local packages: {:?}", local_pkgs);
-
-    // Search for manifests we might have missed, read them, and report any packages
-    for entry in WalkDir::new(current_dir().unwrap()) {
-        let entry = entry.unwrap();
-        if entry.file_name() == "Cargo.toml" {
-            let path = entry.path();
-            debug!("checking manifest: {}", path.display());
-
-            let mut file = try!(File::open(path));
-            let mut manifest = String::new();
-            try!(file.read_to_string(&mut manifest));
-            let pkgname = try!(manifest::package_name_from_manifest(&manifest));
-            debug!("=> {}", pkgname);
-
-            if local_pkgs.iter().find(|name| ***name == pkgname).is_none() {
-                // Missed!
-                return Err(RunError::MissedPkg {
-                    package: String::from(pkgname),
-                    manifest: PathBuf::from(path),
-                });
-            }
-        }
-    }
+    let local_pkgs = try!(collect_local_pkgs(&cargo_args));
 
     // Now run the subcommand for all packages we collected
     for pkg in &local_pkgs {
@@ -180,7 +83,16 @@ fn run() -> Result<(), RunError> {
 fn main() {
     init_logging();
 
-    match run() {
+    let args = App::new("local-pkgs")
+        //.setting(AppSettings::SubcommandRequired)
+        .setting(AppSettings::AllowExternalSubcommands)
+        .arg(Arg::with_name("CARGO_ARGS")
+            .long("cargo-args")
+            .help("Specifies the arguments to pass to all internal cargo invocations. This is \
+                   normally inferred from all passed args."))
+        .get_matches();
+
+    match run(&args) {
         Ok(()) => {},
         Err(e) => {
             let mut stderr = io::stderr();
